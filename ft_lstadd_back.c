e3c0a5c3e8fa2c43f111e45059fcbb7ae6d31e64
Erwan LAJOUANNIQUE <er.lajouannique@gmail.com>
2020-11-16 17:44:53 +0100

    list add back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 12:04:26 by user42            #+#    #+#             */
-/*   Updated: 2020/11/16 12:04:44 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/16 17:44:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-	new->next = NULL;
-	if (*alst == NULL)
+	t_list	*begin;
+
+	if (new)
 	{
-		*alst = new;
-		return ;
+		if (!(*alst))
+			*alst = new;
+		else
+		{
+			begin = *alst;
+			while (begin->next)
+				begin = begin->next;
+			begin->next = new;
+		}
 	}
-	while ((*alst)->next != NULL)
-		*alst = (*alst)->next;
-	(*alst)->next = new;
 }

cddd1a616f7f9df906227586a8b8ea83950e863a
Erwan LAJOUANNIQUE <er.lajouannique@gmail.com>
2020-11-16 15:13:32 +0100

    ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 11:22:18 by user42            #+#    #+#             */
-/*   Updated: 2020/11/16 15:11:21 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/16 15:13:28 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }

9d9ef3fe4be6c07112e5bee90fed35be00696c6b
Erwan LAJOUANNIQUE <er.lajouannique@gmail.com>
2020-11-17 11:11:35 +0100

    copy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 11:27:21 by user42            #+#    #+#             */
-/*   Updated: 2020/11/17 11:09:52 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/17 11:11:25 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ size_t		ft_strlcpy(char *dest, const char *src, size_t dstsize)
 	size_t	lenght_src;
 	char	*source;
 
+	lenght_src = 0;
 	source = (char *)src;
-	if (!src)
+	if (!source)
 		return (0);
-	lenght_src = ft_strlen(source);
+	lenght_src = (size_t)ft_strlen(source);
 	if (dstsize == 0)
 		return (lenght_src);
 	i = 0;

32142f08b7c535f40acc543503e24f0bbad1e0c0
Erwan LAJOUANNIQUE <er.lajouannique@gmail.com>
2020-11-17 10:09:42 +0100

    fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 10:45:22 by user42            #+#    #+#             */
-/*   Updated: 2020/11/16 10:46:30 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/17 10:08:24 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t len)
 	const unsigned char		*source;
 
 	i = 0;
+	if (!dst && !src)
+		return (NULL);
 	dest = (unsigned char*)dst;
 	source = (unsigned char*)src;
 	while (len > 0)

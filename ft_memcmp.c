8f08982e8299668927abd5bfacf26afd09cc704f
Erwan LAJOUANNIQUE <er.lajouannique@gmail.com>
2020-11-17 10:14:14 +0100

    fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 11:04:34 by user42            #+#    #+#             */
-/*   Updated: 2020/11/16 11:05:20 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/17 10:13:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	str2 = (unsigned char*)s2;
 	while (str1 && str2 && n > 0)
 	{
-		if (*str1 - *str2 < 0)
-			return (-1);
-		else if (*str1 - *str2 > 0)
-			return (1);
+		if (*str1 != *str2)
+			return (*str1 - *str2);
 		str1++;
 		str2++;
 		n--;
